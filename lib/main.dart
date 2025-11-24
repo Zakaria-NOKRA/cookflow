fe6436a1ee45158b95f5d65170c5d6b162865d1f
NOKRA Zakaria <zakaria.nokra.ensias@gmail.com>
2025-11-24 16:08:35 +0000

    Initial commit - CookFlow setup

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const CookFlowApp());
+}
+
+class CookFlowApp extends StatelessWidget {
+  const CookFlowApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CookFlow',
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
+}
